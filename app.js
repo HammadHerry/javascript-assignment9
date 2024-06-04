edcb1368f6b3430466473024b7265237707537fa
HammadHerry <hammadherry81@gmail.com>
2024-06-04 06:58:14 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var p = prompt("Enter your first name");
+
+
+var e = prompt("Country" , "china")
+
+
+var yourName = prompt("Enter Your Name");
+
+var s = prompt("karachi" , "The city of light");
+
+
+var q = 'Phone no';
+var j = 'email' ;
+var final = prompt(q+j)
+
+var w = "pakistan is the islamic country";
+var d = prompt(w ,"yes");
+alert("yes pakistan is the islamic country");
